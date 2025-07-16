0eebce2af7ee3a196df0a3474de54ac9857aede0
tomigorn <tomigorn@gmail.com>
2025-07-16 16:19:41 +0200

    add Swagger and add custom root '/' welcome page

--- LoyaltyCards.Server/Program.cs
+++ LoyaltyCards.Server/Program.cs
@@ -1,17 +1,20 @@
-var builder = WebApplication.CreateBuilder(args);
+﻿var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    // ✅ Enable middleware to serve Swagger JSON and UI
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
@@ -19,5 +22,13 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapGet("/", () => Results.Content(
+    @"<html>
+        <body>
+            <h1>Welcome to the LoyaltyCards API</h1>
+            <p>Go to the <a href=""/swagger"">Swagger UI</a> to explore the API.</p>
+        </body>
+      </html>", "text/html"));
 
 app.Run();
