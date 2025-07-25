c2b954c717fd27ff1d18a3cfdcc2ccaea22d44e8
tomigorn <tomigorn@gmail.com>
2025-07-25 16:05:38 +0200

    create Shared Library for Password Complexity Check

---
+++ LoyaltyCards.Shared/PasswordComplexityAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace LoyaltyCards.Shared
+{
+    public class PasswordComplexityAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object? value)
+        {
+            if (value is not string password) return false;
+
+            var pattern = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z0-9]).{8,}$";
+            return Regex.IsMatch(password, pattern);
+        }
+
+        public override string FormatErrorMessage(string name)
+        {
+            return "Password must be at least 8 characters long and include uppercase, lowercase, number, and special character.";
+        }
+    }
+}
