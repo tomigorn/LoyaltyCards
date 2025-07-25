cc23301eba7d6a00852e226e791a27910863401d
tomigorn <tomigorn@gmail.com>
2025-07-25 14:25:10 +0200

    improve key derive function

--- LoyaltyCards.Server/Helpers/AesEncryptionHelper.cs
+++ LoyaltyCards.Server/Helpers/AesEncryptionHelper.cs
@@ -17,8 +17,12 @@ namespace LoyaltyCards.Server.Helpers
     {
         public static byte[] DeriveKey(string password, byte[] salt)
         {
-            using var pbkdf2 = new Rfc2898DeriveBytes(password, salt, 100_000, HashAlgorithmName.SHA256);
-            return pbkdf2.GetBytes(32);
+            return Rfc2898DeriveBytes.Pbkdf2(
+                password,
+                salt,
+                100000,
+                HashAlgorithmName.SHA256,
+                32);
         }
 
         public static (byte[] Ciphertext, byte[] Nonce, byte[] Tag) Encrypt(string plaintext, byte[] key)
