39529ed32d7f307aebb5f6c046ce0038bc7fdbe0
tomigorn <tomigorn@gmail.com>
2025-09-08 16:09:17 +0200

    Create LoyaltyCard Service

---
+++ Backend/LoyaltyCards.Application/Services/LoyaltyCardService.cs
@@ -0,0 +1,27 @@
+using LoyaltyCards.Domain.Entities;
+using LoyaltyCards.Infrastructure.Persistence;
+
+public class LoyaltyCardService
+{
+    private readonly LoyaltyCardRepository _repository;
+
+    public LoyaltyCardService(LoyaltyCardRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public void CreateCard(string nickname, string storeName, string barcodeNumber, Guid userId)
+    {
+        var card = new LoyaltyCard
+        {
+            Id = Guid.NewGuid(),
+            Nickname = nickname,
+            StoreName = storeName,
+            BarcodeNumber = barcodeNumber,
+            UserId = userId,
+            CreationDate = DateTime.UtcNow
+        };
+
+        _repository.Save(card);
+    }
+}
