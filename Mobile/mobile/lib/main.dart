1d77daa0ee0e4a747a3f9dfacde1f01b85876f00
tomigorn <tomigorn@gmail.com>
2025-09-08 14:04:51 +0200

    Linux: phone sized window

--- Mobile/mobile/lib/main.dart
+++ Mobile/mobile/lib/main.dart
@@ -2,15 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'screens/login_screen.dart';
 import 'api_config.dart';
+import 'package:flutter/foundation.dart';
+import 'package:window_size/window_size.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
-  
+
+  if (defaultTargetPlatform == TargetPlatform.linux) {
+    const double phoneWidth = 400;
+    const double phoneHeight = 800;
+    setWindowTitle('My App');
+    setWindowMinSize(const Size(phoneWidth, phoneHeight));
+    setWindowMaxSize(const Size(phoneWidth, phoneHeight));
+    setWindowFrame(const Rect.fromLTWH(100, 100, phoneWidth, phoneHeight));
+  }
+
   // Get environment from --dart-define or default to 'dev'
   final environment = const String.fromEnvironment('ENV', defaultValue: 'dev');
   await ApiConfig.load(environment);
-  
+
   runApp(const MyApp());
 }
 
