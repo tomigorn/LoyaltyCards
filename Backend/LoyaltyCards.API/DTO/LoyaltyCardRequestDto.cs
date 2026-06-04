380a924b50bd8bf881b6cecaad9b556b7c714fe3
tomigorn <tomigorn@gmail.com>
2026-06-04 10:51:47 +0200

    LoyaltyCards: empty repository for fresh start

--- Backend/LoyaltyCards.API/DTO/LoyaltyCardRequestDto.cs
+++
@@ -1,6 +0,0 @@
-public class LoyaltyCardRequestDto
-{
-    public string Nickname { get; set; } = null!;
-    public string? StoreName { get; set; }
-    public string BarcodeNumber { get; set; } = null!;
-}
